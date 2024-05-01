58a1d168b5eccd780a156c2eb97f549d87ed936e
Kayla-Ard <kaylard@yahoo.com>
2024-05-01 12:34:54 -0600

    2nd homework assignment

--- homework/homework_python_lists.py
+++ homework/homework_python_lists.py
@@ -39,6 +39,6 @@ print(temperatures[7:14])
 temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
 hot_temps = []
 for temp in temperatures:
-    if temp >= 100:
+    if temp > 100:
         hot_temps.append(temp)
 print(hot_temps)
--- homework/homework_python_loop_statements.py
+++ homework/homework_python_loop_statements.py
@@ -1,7 +1,26 @@
 # The Range Riddle - Task 1: Your Mood Today
+# Write a program that prints off different moods for each day of the week. 
+# Create a list of moods such as "Happy", "Sad", "Energetic", and "Calm". 
+# Using the range() function, loop through the days of the week and for each day, randomly select a mood from the list and print it.
+# Ensure that your program includes the use of the random module to select the mood.
 
-
+import random
+moods = ["Happy", "Sad", "Mad", "Energetic", "Calm", "Lonely", "Hyper"]
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+for i in range(7):
+        print(f"On {days_of_week[i]} I was feeling {random.choice(moods)}.")
 
 
 
 # Double Scoop with Nested Loops - Task 1: Your Mood Tracker
+# Simulate a mood tracker that records your mood at three different times of the day (morning, afternoon, evening) for each day of the week.
+# Use nested loops to implement this: the outer loop should iterate over the days, and the inner loop should iterate over the times of the day.
+# For each time, randomly select a mood from a predefined list and print it.
+
+import random
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+times_of_day = ["Morning", "Afternoon", "Evening"]
+moods = ["Happy", "Sad", "Mad", "Energetic", "Calm", "Lonely", "Hyper"]
+for day in days_of_week:
+    for time in times_of_day:
+        print(f" On {day} {time} I was {random.choice(moods)}.")
